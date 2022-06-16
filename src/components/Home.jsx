cbddce9d6e416b74155f751e52340efc071fc498
Daniel Madaghjian <danielmadaghjian@gmail.com>
2022-06-16 19:17:18 +0100

    created home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-600'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+          Daniel Madaghjian
+        </h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+          I'm a Computer Science and Business Student.
+        </h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>
+          I’m a computer science and business student in Trinity College Dublin, with a passion for technology and business. 
+          Currently I am enrolled as Summer intern in Deloitte working in the consulting department. 
+        </p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            View Work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3 ' />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
