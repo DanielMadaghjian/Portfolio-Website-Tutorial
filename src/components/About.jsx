cc1a3797a146aca6e81f72a3320d6beabd20f72d
Daniel Madaghjian <danielmadaghjian@gmail.com>
2022-06-17 17:50:42 +0100

    update

---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      <div className='flex flex-col justify-center items-center w-full h-full'>
+        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600'>
+              About
+            </p>
+          </div>
+          <div></div>
+          </div>
+          <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>Hi. I'm Daniel, thanks for coming. Please take a look around!</p>
+            </div>
+            <div>
+              <p>I have a huge passion for technology and business, which led me to undertake my degree in computer science and business
+                in Trinity College Dublin. I have a strong foundation in many programming languages. Through college I learned Java, C# and C. In addition, through 
+                the startup I have been involved in in the last year 'YourTutor', I have adapted to other languages such as Vue, HTML and CSS. 
+                By working with YourTutor it also built my knowledge of business.</p>  
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
